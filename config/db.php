f0c473f44580516c1fbe51501e96482e7595931e
salgadd <salgadd@gmail.com>
2016-01-08 07:54:12 +0700

    Edit DB Connection

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=categorized_alquran',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'salgadd',
+    'password' => 'salgadd123',
     'charset' => 'utf8',
 ];
