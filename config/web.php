c0fe05b99ea24b6fc93bdab25dac511ffd4a5fbe
salgadd <salgadd@gmail.com>
2016-01-08 07:54:32 +0700

    Add Pretty URL (again)

--- config/web.php
+++ config/web.php
@@ -8,10 +8,10 @@ $config = [
     'bootstrap' => ['log'],
     'components' => [
         //enable pretty url wamp
-		//'urlManager' => [
-		//	'showScriptName' => false,
-		//	'enablePrettyUrl' => true
-		//],
+		'urlManager' => [
+			'showScriptName' => false,
+			'enablePrettyUrl' => true
+		],
 		'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '123',
